b1c49659f5d3e185d1cd1c880b3d0a21d0b9281d
Roy van Kaathoven <royvankaathoven@hotmail.com>
2012-12-15 14:40:04 +0100

    Fixed autoloading configuration

--- src/RovakCodeMirror/Module.php
+++ src/RovakCodeMirror/Module.php
@@ -42,7 +42,7 @@ class Module implements
        return array(
             AutoloaderFactory::STANDARD_AUTOLOADER => array(
                 StandardAutoloader::LOAD_NS => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__,
                 ),
             ),
         );
